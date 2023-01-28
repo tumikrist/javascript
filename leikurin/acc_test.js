0f42c4cdcc1c3adecb7d1ce56eb7c5eb0f713824
tumikrist <tumikr20@tskoli.is>
2023-01-28 18:17:49 +0000

    nyjasta

--- leikurin/acc_test.js
+++ leikurin/acc_test.js
@@ -2,15 +2,38 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const angle = Math.PI / 180;
 const score_keeper = document.getElementById('player_score');
+const life_maneger = document.getElementById('player_lives');
 
-const pacman ={
+let counter = 0;
+
+let ghost_counter = 0;
+
+function ghost_timer_for_touch() {
+    ghost_counter = 0.5;
+    pacman_touched_ghost = true;
+    let intervalId = setInterval(() => {
+        if (ghost_counter <= 0) {
+            clearInterval(intervalId);
+            console.log("Countdown complete!");
+            pacman_touched_ghost = false;
+        } else {
+            console.log(ghost_counter);
+            ghost_counter--;
+        }
+    }, 1000);
+}
+
+
+const pacman = {
     x: 400,
     y: 400,
     radius: 25,
     color: 'yellow',
     pacman_speed: 2,
+    pacman_life: 3,
     angle_1: 30,
     angle_2: 330,
+    can_kill_ghost: false,
     draw() {
         ctx.beginPath();
         ctx.strokeStyle = 'black';
@@ -21,6 +44,31 @@ const pacman ={
         ctx.closePath();
         ctx.stroke();
         ctx.fill();
+    },
+    pacman_power_pellet_effect() {
+        counter += 4;
+        this.can_kill_ghost = true;
+        this.color = 'blue';
+        ghost.color_main = 'rgb(0,102,204)';
+
+        let intervalId = setInterval(() => {
+        if (counter === 0) {
+            clearInterval(intervalId);
+            this.can_kill_ghost = false;
+            this.color = 'yellow';
+            ghost.color_main = ghost.color_main_copy;
+            console.log("Countdown complete!");
+        } else if(counter % 2 == 0) {
+            console.log(counter+"slett");
+            ghost.color_main = ghost.color_main_copy;
+            counter--;
+        } else if (counter % 2 == 1) {
+            console.log(counter+"odda");
+            ghost.color_main = ghost.color_main_copy;
+            ghost.color_main = 'rgb(0,102,204)';
+            counter--;
+        }
+        }, 1000);
     }
 };
 
@@ -84,10 +132,84 @@ const power_pellet = {
         let sumofradii = pacman.radius + pellet.radius;
         if (distance < sumofradii) {
             console.log('collision');
+            pacman.pacman_power_pellet_effect();
             this.x = 1000;
             this.y = 1000;
             player_score += 1;
             score_keeper.innerHTML = "score : "+player_score;
+        }   
+    }
+}
+
+pacman_touched_ghost = false;
+
+const ghost = {
+    x: 325,
+    y: 200,
+    radius: 25,
+    color_main: 'red',
+    color_main_copy: 'red',
+    color_lines: 'black',
+    color_eye: 'white',
+    ghost_speed: 5,
+    draw() {
+        ctx.beginPath();
+        ctx.fillStyle = this.color_main;
+        ctx.strokeStyle = this.color_lines;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x, this.y, this.radius, angle * 180, angle * 0, false);
+        let temp = this.x + 25;
+        for (let i = 0; i < 11; i++) {
+            if (i % 2 == 0) {
+                ctx.lineTo(temp, this.y + 50);
+            } else {
+                ctx.lineTo(temp, this.y + 40);
+            }
+            temp -= 5;
+        }
+        ctx.closePath();
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.strokeStyle = this.color_lines;
+        ctx.fillStyle = this.color_eye;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x - 12.5, this.y, 5, angle * 0, angle * 360, false);
+        ctx.fill();
+        ctx.stroke();
+
+        ctx.beginPath();
+        ctx.strokeStyle = this.color_lines;
+        ctx.fillStyle = this.color_eye;
+        ctx.lineWidth = 3;
+        ctx.arc(this.x + 12.5, this.y, 5, angle * 0, angle * 360, false);
+        ctx.fill();
+        ctx.stroke();
+    },
+    tracker() {
+        let dx = pacman.x - this.x;
+        let dy = pacman.y - this.y;
+        let distance = Math.sqrt(dx * dx + dy * dy);
+        let sumofradii = pacman.radius + this.radius;
+        if (distance < sumofradii) {
+            if(pacman.can_kill_ghost === true) {
+                this.x = Math.floor(Math.random() * 750);
+                this.y = Math.floor(Math.random() * 750);
+                player_score += 1;
+                score_keeper.innerHTML = "score : "+player_score;
+                console.log('die');
+            } else  {
+                if (pacman_touched_ghost === false) {
+                    ghost_timer_for_touch();
+                    pacman.pacman_life -=1;
+                    console.log('die pacman'+pacman.pacman_life);
+                    life_maneger.innerHTML = "Lives : "+pacman.pacman_life;
+                } else {
+                    console.log('no lives lost pacman');
+                }
+            }
+            //console.log('collision');
         }   
     }
 }
@@ -114,7 +236,7 @@ document.addEventListener('keydown', (event) => {
 }, false);
 
 //move pacman
-function move(){
+function move() {
     if ((arrowdown? 1:0) + (arrowup? 1:0) + (arrowleft? 1:0) + 
     (arrowright? 1:0) == 2) {
         if ( arrowright === true && arrowup === true) {
@@ -182,6 +304,7 @@ document.addEventListener('keyup', (event) => {
 }, false);
 
 
+// veit ekki hvort eg þarf þetta
 function colision(){
     /* let dx = pacman.x - pellet.x;
     let dy = pacman.y - pellet.y;
@@ -198,9 +321,12 @@ function colision(){
         //console.log('not touching');
     } */
 }
+// veit ekki hvort eg þarf þetta end
 
 player_score = 0;
 
+
+// bua til shit 
 const pellet1 = Object.create(pellet);
 const pellet2 = Object.create(pellet);
 const pellet3 = Object.create(pellet);
@@ -238,10 +364,15 @@ power_pellet3.y = 20;
 
 power_pellet4.x = 20;
 power_pellet4.y = 780;
+// bua til shit end
+
 
 function init(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     pacman.draw();
+    ghost.draw();
+    ghost.tracker();
+
     for (let i = 0; i < pellets.length; i++) {
         pellets[i].draw();
         pellets[i].tracker();
