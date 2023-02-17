3e7e5ccf491ea478c2fa715ac22010bbb33522af
tumikrist <tumikr20@tskoli.is>
2023-02-17 20:24:22 +0000

    lol

--- vefapp/code.js
+++ vefapp/code.js
@@ -15,12 +15,33 @@ async function getUsers() {
 async function renderUsers() {
     let users = await getUsers();
     let html = '';
-    users.forEach(user => {
+
+    let user_listi = users
+    console.log(user_listi)
+
+    /* delete user_listi[0]
+    for(let i = 0; i < 7; i++){
+        delete user_listi[i]
+    }
+    console.log(user_listi) */
+    const value = document.querySelector("#value")
+    const input = document.querySelector("#pi_input")
+    value.textContent = input.value
+    input.addEventListener("input", (event) => {
+        value.textContent = event.target.value
+    })
+
+    user_listi.forEach(user => {
         let htmlSegment = `<div class="user">
                             <div class="mynd">
-                                <img src="${user.web_photo}" >
+                                <img src="${user.web_photo}" ></img>
                             </div>
-                            <h2>${user.name}</h2>
+                            <div class="texti">
+                                <h2>Name : ${user.name}</h2>
+                                <h2>Price : ${user.price}</h2>
+                                <h2>Date : ${user.date}</h2>
+                                <h2>Location : ${user.location}</h2>
+                            </div>
                         </div>`;
 
         html += htmlSegment;
@@ -30,5 +51,16 @@ async function renderUsers() {
     container.innerHTML = html;
 }
 
+
+/* const value = document.querySelector("#value")
+
+value.textContent = input.value
+input.addEventListener("input", (event) => {
+    value.textContent = event.target.value
+}) */
+
+
 renderUsers();
 
+//<h2>${user.name}</h2>
+{/* <img src="${user.web_photo}" ></img> */}
